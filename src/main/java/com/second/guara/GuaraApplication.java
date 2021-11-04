85a1ed6b8db4a21ff79b8bfe3ad242e2faa20632
2nd_element <2781372804@qq.com>
2021-11-04 23:18:52 +0800

    Initial commit

---
+++ src/main/java/com/second/guara/GuaraApplication.java
@@ -0,0 +1,13 @@
+package com.second.guara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuaraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuaraApplication.class, args);
+    }
+
+}
